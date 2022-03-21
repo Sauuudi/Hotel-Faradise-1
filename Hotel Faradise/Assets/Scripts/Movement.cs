88106b5b96f283081ea5e9f0927bb6b6cf2a2340
LuKmf <lucas.fur99@gmail.com>
2022-03-21 13:22:32 +0100

    Animaciones Connor

--- Hotel Faradise/Assets/Scripts/Movement.cs
+++ Hotel Faradise/Assets/Scripts/Movement.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 4.5f; 
+    public float speed = 2f; 
     public float jumpForce = 5.0f;
 
     private Rigidbody2D _body;
+    private Animator _anim;
+    private BoxCollider2D _box;
 
     void Start()
     {
         _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
     }
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+        if (!Mathf.Approximately(deltaX, 0f))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1f, 1f);
+        }
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
+            _anim.SetBool("jumping", true);
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private (Vector2, Vector2) getGroundCheckCorners()
+    {
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        return (corner1, corner2);
+    }
+    public bool grounded
+    {
+        get
+        {
+            var (corner1, corner2) = getGroundCheckCorners();
+            Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+            _anim.SetBool("jumping", false);
+            return (hit != null);
         }
     }
 }
