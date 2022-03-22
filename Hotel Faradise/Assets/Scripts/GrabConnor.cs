b0d35d320f3cfa67db7c8fed6f894bd39e51429a
LuKmf <lucas.fur99@gmail.com>
2022-03-22 14:44:45 +0100

    Grab Connor
    Mecánica y animación

---
+++ Hotel Faradise/Assets/Scripts/GrabConnor.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabConnor : MonoBehaviour
+{
+    public Transform _grabDetect;
+    public Transform _boxHolder;
+    public Animator _anim;
+
+    public float rayDistance;
+
+    void Update()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(_grabDetect.position, Vector2.right * transform.localScale, rayDistance);
+
+        if(grabCheck.collider != null && grabCheck.collider.tag == "Box")
+        {
+            if(Input.GetKey(KeyCode.C))
+            {
+                _anim.SetBool("grabbing", true);
+                grabCheck.collider.gameObject.transform.parent = _boxHolder;
+                grabCheck.collider.gameObject.transform.position = _boxHolder.position;
+                //Physhics off
+                //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+            else
+            {
+                _anim.SetBool("grabbing", false);
+                grabCheck.collider.gameObject.transform.parent = null;
+                //Physhics on
+                //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+        }
+    }
+}
