3d641b4b4ca0071e05b1943119d5de13e2edc115
Sauuudi <iliasbox2@gmail.com>
2022-03-24 23:32:23 +0100

    Suelo deslizante
    ya hay suelo deslizante, el jugador va deslizandose hasta parar por la friccion(podemos quiatrla), no puede moverse ni saltar cuando esta sobre hielo, eso hay que cambiarlo luego

--- Hotel Faradise/Assets/Scripts/Movement.cs
+++ Hotel Faradise/Assets/Scripts/Movement.cs
@@ -7,19 +7,21 @@ public class Movement : MonoBehaviour
 {
     public float speed = 2f; 
     public float jumpForce = 5.0f;
+    public float iceSpeed;
 
     private Rigidbody2D _body;
     private Animator _anim;
     private BoxCollider2D _box;
 
-    [SerializeField] private LayerMask m_WhatIsGround;							// A mask determining what is ground to the character
-    [SerializeField] private Transform m_GroundCheck;		// A position marking where to check if the player is grounded.
+    [SerializeField] private LayerMask m_WhatIsGround; // A mask determining what is ground to the character
+    [SerializeField] private LayerMask m_WhatIsIce; // A mask determining what is ground to the character
+    [SerializeField] private Transform m_GroundCheck;	// A position marking where to check if the player is grounded.
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;   // Whether or not the player is grounded.
-
-    public float iceSpeed;
-    private bool onIce;
+    private bool m_onIce;
+    
     public UnityEvent OnLandEvent;
+    public UnityEvent OnIceEvent;
 
     void Start()
     {
@@ -27,16 +29,18 @@ public class Movement : MonoBehaviour
         _anim = GetComponent<Animator>();
         _box = GetComponent<BoxCollider2D>();
         iceSpeed = 20;
-        onIce = false;
     }
 
     private void Awake() {
         if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
+
+        if (OnIceEvent == null)
+			OnIceEvent = new UnityEvent();
     }
     void Update()
     {   
-        if(!onIce){
+        if(!m_onIce){
             float deltaX = Input.GetAxis("Horizontal") * speed ;
             _anim.SetFloat("speed", Mathf.Abs(deltaX));
             if (!Mathf.Approximately(deltaX, 0f))
@@ -47,28 +51,23 @@ public class Movement : MonoBehaviour
             _body.velocity = movement;  
         }
 
-        if (m_Grounded && !onIce && Input.GetKeyDown(KeyCode.Space))
+        if (m_Grounded && !m_onIce && Input.GetKeyDown(KeyCode.Space))
         {
             _anim.SetTrigger("jumping");
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
-    
-        //aqui tendria que mirar si esta sobre ice, en vez de pulsar la p
-        // yo haria que los suelos de hielotengan un tag iceGround
-        //si esta sobre hielo, desactivar el movimiento normal
-        if ((Input.GetKeyDown("p"))) 
-        { 
-            if(onIce) onIce = false;
-            else onIce = true;
-        }
-        
-        //cuando este sobre hielo aplicar un movimiento continuo sobre el personaje
-        
+        }    
+
+        if(m_onIce){
+            Debug.Log("estoy en hielo xdd");
+        }    
     }
 
     private void FixedUpdate() {
         bool wasGrounded = m_Grounded;
 		m_Grounded = false;
+
+        bool wasOnIce = m_onIce;
+		m_onIce = false;        
 
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
@@ -77,15 +76,25 @@ public class Movement : MonoBehaviour
 		{
 			if (colliders[i].gameObject != gameObject)
 			{
-                Debug.Log("es grounded");
 				m_Grounded = true;
 				if (!wasGrounded)
 					OnLandEvent.Invoke();
 			}
 		}
+
+        Collider2D[] collidersIce = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsIce);
+        for (int i = 0; i < collidersIce.Length; i++)
+		{
+			if (collidersIce[i].gameObject != gameObject)
+			{
+				m_onIce = true;
+				if (!wasOnIce)
+					OnLandEvent.Invoke();
+			}
+		}
     }
 
-    private (Vector2, Vector2) getGroundCheckCorners()
+    /*private (Vector2, Vector2) getGroundCheckCorners()
     {
         Vector3 max = _box.bounds.max;
         Vector3 min = _box.bounds.min;
@@ -93,7 +102,7 @@ public class Movement : MonoBehaviour
         Vector2 corner2 = new Vector2(min.x, min.y - .2f);
         return (corner1, corner2);
     }
-    /*public bool grounded
+    public bool grounded
     {
         get
         {
