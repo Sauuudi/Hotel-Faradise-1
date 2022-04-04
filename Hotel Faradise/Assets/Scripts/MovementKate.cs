dbe9a13d0c76d01539061717b40477e74376873f
Sauuudi <iliasbox2@gmail.com>
2022-04-04 17:06:47 +0200

    move on ice
    ya puede moverse y saltar en hielo

--- Hotel Faradise/Assets/Scripts/MovementKate.cs
+++ Hotel Faradise/Assets/Scripts/MovementKate.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 
 public class MovementKate : MonoBehaviour
 {
-    public float speed = 2f; 
+    public float speed = 2f;
     public float jumpForce = 5.0f;
     public float iceSpeed;
 
@@ -17,9 +17,9 @@ public class MovementKate : MonoBehaviour
     [SerializeField] private LayerMask m_WhatIsIce; // A mask determining what is ground to the character
     [SerializeField] private Transform m_GroundCheck;	// A position marking where to check if the player is grounded.
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-	private bool m_Grounded;   // Whether or not the player is grounded.
+    private bool m_Grounded;   // Whether or not the player is grounded.
     private bool m_onIce;
-    
+    private Vector3 lastPos;
     public UnityEvent OnLandEvent;
     public UnityEvent OnIceEvent;
 
@@ -29,18 +29,21 @@ public class MovementKate : MonoBehaviour
         _anim = GetComponent<Animator>();
         _box = GetComponent<BoxCollider2D>();
         iceSpeed = 20;
+        lastPos = transform.position;
     }
 
-    private void Awake() {
+    private void Awake()
+    {
         if (OnLandEvent == null)
-			OnLandEvent = new UnityEvent();
+            OnLandEvent = new UnityEvent();
 
         if (OnIceEvent == null)
-			OnIceEvent = new UnityEvent();
+            OnIceEvent = new UnityEvent();
     }
-    void Update()
-    {   
-        if(!m_onIce){
+    async void Update()
+    {
+        if (!m_onIce)
+        {
             float deltaX = 0f;
             if (Mathf.Abs(Input.GetAxis("Horizontal_originalK") * speed) > Mathf.Abs(Input.GetAxis("Horizontal joyconL joystick") * speed))
             {
@@ -57,68 +60,90 @@ public class MovementKate : MonoBehaviour
                 transform.localScale = new Vector3(Mathf.Sign(deltaX), 1f, 1f);
             }
             Vector2 movement = new Vector2(deltaX, _body.velocity.y);
-            _body.velocity = movement;  
+            _body.velocity = movement;
         }
+        else if (!isMoving())
+        {
+            float deltaX = 0f;
+            if (Mathf.Abs(Input.GetAxis("Horizontal_originalK") * speed) > Mathf.Abs(Input.GetAxis("Horizontal joyconL joystick") * speed))
+            {
+                deltaX = Input.GetAxis("Horizontal_originalK") * speed;
+            }
+            else
+            {
+                deltaX = Input.GetAxis("Horizontal joyconL joystick") * speed;
+            }
+
+            _anim.SetFloat("speed", Mathf.Abs(deltaX));
+            if (!Mathf.Approximately(deltaX, 0f))
+            {
+                transform.localScale = new Vector3(Mathf.Sign(deltaX), 1f, 1f);
+            }
+            Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+            _body.velocity = movement;
+        }
+
+
+
 
         if (m_Grounded && !m_onIce && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Joystick1Button0)))
         {
             _anim.SetTrigger("jumping");
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }    
+        }
+        else if (m_onIce && !isMoving() && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Joystick1Button0)))
+        {
+            _anim.SetTrigger("jumping");
+            _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
 
-        if(m_onIce){
+        if (m_onIce)
+        {
             Debug.Log("estoy en hielo xdd");
-        }    
+        }
+        if (m_Grounded)
+        {
+            Debug.Log("estoy en el suelo ");
+        }
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         bool wasGrounded = m_Grounded;
-		m_Grounded = false;
+        m_Grounded = false;
 
         bool wasOnIce = m_onIce;
-		m_onIce = false;        
+        m_onIce = false;
 
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-		for (int i = 0; i < colliders.Length; i++)
-		{
-			if (colliders[i].gameObject != gameObject)
-			{
-				m_Grounded = true;
-				if (!wasGrounded)
-					OnLandEvent.Invoke();
-			}
-		}
+        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+                if (!wasGrounded)
+                    OnLandEvent.Invoke();
+            }
+        }
 
         Collider2D[] collidersIce = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsIce);
         for (int i = 0; i < collidersIce.Length; i++)
-		{
-			if (collidersIce[i].gameObject != gameObject)
-			{
-				m_onIce = true;
-				if (!wasOnIce)
-					OnLandEvent.Invoke();
-			}
-		}
+        {
+            if (collidersIce[i].gameObject != gameObject)
+            {
+                m_onIce = true;
+                if (!wasOnIce)
+                    OnLandEvent.Invoke();
+            }
+        }
     }
 
-    /*private (Vector2, Vector2) getGroundCheckCorners()
+    private bool isMoving()
     {
-        Vector3 max = _box.bounds.max;
-        Vector3 min = _box.bounds.min;
-        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
-        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
-        return (corner1, corner2);
+        return _body.velocity.magnitude > 0.3;
     }
-    public bool grounded
-    {
-        get
-        {
-            var (corner1, corner2) = getGroundCheckCorners();
-            Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
-            return (hit != null);
-        }
-    }*/
-    
+
+
 }
