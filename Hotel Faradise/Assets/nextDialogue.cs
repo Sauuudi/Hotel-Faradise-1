d6fceea316e9b7ad25167d5d9729ebf968071783
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2022-04-12 22:33:58 +0200

    Más mapeado de botones

---
+++ Hotel Faradise/Assets/nextDialogue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nextDialogue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button1) || Input.GetKey("q") ||
+            Input.GetKey(KeyCode.Joystick2Button1) || Input.GetKey("m") ||
+            Input.GetKeyDown(KeyCode.Joystick2Button0) || Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            //Debug.Log("hola");
+            this.GetComponent<Button>().onClick.Invoke();
+        }
+    }
+}
