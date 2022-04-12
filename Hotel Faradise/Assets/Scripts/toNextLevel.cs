596bea6781e06af6fa01e8d13cac91a12ef2aee7
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2022-04-12 12:31:45 +0200

    Next Level Update

--- Hotel Faradise/Assets/Scripts/toNextLevel.cs
+++ Hotel Faradise/Assets/Scripts/toNextLevel.cs
@@ -6,44 +6,61 @@ using UnityEngine.SceneManagement;
 
 public class toNextLevel : MonoBehaviour
 {
-    public Image Mask;
-    public Image Circle;
+    public Transform Mask;
+    public GameObject Circle;
     private bool pressC = false;
     private bool pressK = false;
     private float totalAmount = 0f;
     private bool onCoroutine = false;
+    private Material mySpriteMaterial;
+    private float originalX;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        mySpriteMaterial = Circle.GetComponent<SpriteRenderer>().material;
+        originalX = Mask.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         totalAmount = 0f;
-        if (Input.GetKey(KeyCode.Joystick1Button1))
+        if (Input.GetKey(KeyCode.Joystick1Button1) || Input.GetKey("q"))
         {
             pressK = true;
             totalAmount += 0.5f;
         }
         else
         {
-            Circle.fillAmount = 0f;
+            mySpriteMaterial.SetFloat("_FillAmount", 0f);
+            //Circle.fillAmount = 0f;
             pressK = false;
         }
-        if (Input.GetKey(KeyCode.Joystick2Button1))
+        if (Input.GetKey(KeyCode.Joystick2Button1) || Input.GetKey("m"))
         {
             pressC = true;
             totalAmount += 0.5f;
         }
         else
         {
-            Circle.fillAmount = 0f;
+            mySpriteMaterial.SetFloat("_FillAmount", 0f);
+            //Circle.fillAmount = 0f;
             pressC = false;
         }
-        Mask.fillAmount = totalAmount;
+        switch (totalAmount)
+        {
+            case (0.5f):
+                Mask.position = new Vector3(originalX + 0.205f, Mask.position.y, Mask.position.z);
+                break;
+            case (1f):
+                Mask.position = new Vector3(originalX + 0.4f, Mask.position.y, Mask.position.z);
+                break;
+            default:
+                Mask.position = new Vector3(originalX, Mask.position.y, Mask.position.z);
+                break;
+        }
+        //Mask.transform.position = totalAmount;
         if (pressK && pressC && !onCoroutine)
         {
             StartCoroutine(CompleteCircle());
@@ -54,11 +71,12 @@ public class toNextLevel : MonoBehaviour
     {
         onCoroutine = true;
         bool release = false;
-        while (!release && pressC && pressK && Circle.fillAmount < 1f)
+        while (!release && pressC && pressK && mySpriteMaterial.GetFloat("_FillAmount") < 1f)
         {
             if(pressC && pressK)
             {
-                Circle.fillAmount += Time.deltaTime;
+                mySpriteMaterial.SetFloat("_FillAmount", mySpriteMaterial.GetFloat("_FillAmount") + Time.deltaTime);
+                //Circle.fillAmount += Time.deltaTime;
                 yield return null;
             } else
             {
@@ -68,6 +86,6 @@ public class toNextLevel : MonoBehaviour
             
         }
         onCoroutine = false;
-        if (!release && Circle.fillAmount >= 1) SceneManager.LoadScene(3);
+        if (!release && mySpriteMaterial.GetFloat("_FillAmount") >= 1) SceneManager.LoadScene(3);
     }
 }
