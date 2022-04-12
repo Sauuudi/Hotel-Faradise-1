712954c36a8a0545fa232a7d34608d40e789b23c
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2022-04-12 18:29:00 +0200

    Arreglando cosas

--- Hotel Faradise/Assets/Scripts/Character/GrabConnor.cs
+++ Hotel Faradise/Assets/Scripts/Character/GrabConnor.cs
@@ -7,9 +7,14 @@ public class GrabConnor : MonoBehaviour
     public Transform _grabDetect;
     public Transform _boxHolder;
     public Animator _anim;
-
+    private bool grabbing = false;
     public float rayDistance;
-
+    private GameObject boxTaken;
+    private Rigidbody2D body;
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         RaycastHit2D grabCheck = Physics2D.Raycast(_grabDetect.position, Vector2.right * transform.localScale, rayDistance);
@@ -18,19 +23,46 @@ public class GrabConnor : MonoBehaviour
         {
             if(Input.GetKey(KeyCode.C))
             {
-                _anim.SetBool("grabbing", true);
-                grabCheck.collider.gameObject.transform.parent = _boxHolder;
-                grabCheck.collider.gameObject.transform.position = _boxHolder.position;
-                //Physhics off
-                //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            }
+                if (!grabbing)
+                {
+                    boxTaken = grabCheck.collider.gameObject;
+                    grabbing = true;
+                    _anim.SetBool("grabbing", true);
+                    boxTaken.transform.parent = _boxHolder;
+
+                    //boxTaken.transform.position = _boxHolder.position;
+                    boxTaken.GetComponent<HingeJoint2D>().connectedBody = body;
+                    boxTaken.GetComponent<HingeJoint2D>().enabled = true;
+                    this.GetComponent<MovementConnor>().jumpForce = this.GetComponent<MovementConnor>().jumpForce + 18;
+
+                    boxTaken.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+                    //grabCheck.collider.gameObject.transform.parent = _boxHolder;
+                    //grabCheck.collider.gameObject.transform.position = _boxHolder.position;
+                    //Physhics off
+                    //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                }
+            } /*
             else
             {
+                grabbing = false;
                 _anim.SetBool("grabbing", false);
-                grabCheck.collider.gameObject.transform.parent = null;
+                boxTaken.transform.parent = null;
+                //grabCheck.collider.gameObject.transform.parent = null;
                 //Physhics on
                 //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-            }
+            } */
+        }
+
+        if(grabbing && !Input.GetKey(KeyCode.C))
+        {
+            this.GetComponent<MovementConnor>().jumpForce = this.GetComponent<MovementConnor>().jumpForce - 18;
+            grabbing = false;
+            _anim.SetBool("grabbing", false);
+            boxTaken.transform.parent = null;
+            boxTaken.GetComponent<HingeJoint2D>().enabled = false;
+            boxTaken.GetComponent<HingeJoint2D>().connectedBody = null;
+            boxTaken.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            //boxTaken.GetComponent<Rigidbody2D>().isKinematic = false;
         }
     }
 }
--- Hotel Faradise/Assets/Scripts/resetBalance.cs
+++ Hotel Faradise/Assets/Scripts/resetBalance.cs
@@ -5,18 +5,9 @@ using UnityEngine;
 public class resetBalance : MonoBehaviour
 {
 
-    private Vector3 initialPos;
-    [SerializeField]
-    private int notColliding;
-    private bool restart = false;
     public GameObject upperPlatform;
     public int id;
     // Start is called before the first frame update
-    void Start()
-    {
-        initialPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-    }
-
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
