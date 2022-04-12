0e39634822a1525206c00c724abf858040ba3c42
Catacumbas-NPC <nicolasbener@gmail.com>
2022-04-12 19:50:51 +0200

    Volumen en las dos primeras escenas

--- Hotel Faradise/Assets/Scripts/Menus/PauseMenu.cs
+++ Hotel Faradise/Assets/Scripts/Menus/PauseMenu.cs
@@ -32,6 +32,7 @@ public class PauseMenu : MonoBehaviour {
     }
 
     public void mainMenu(){
-       SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
     }
 }
