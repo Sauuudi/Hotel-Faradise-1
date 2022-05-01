7979da898a23b72e1eaed012d862eec8130f84cf
DiegoRN <83607922+DiegoRN@users.noreply.github.com>
2022-05-01 12:43:01 +0200

    Respawn para cajas

--- Hotel Faradise/Assets/Scripts/RespawnTP.cs
+++ Hotel Faradise/Assets/Scripts/RespawnTP.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class RespawnTP : MonoBehaviour
 {
     public GameObject respawnPoint;
+    public string tagName = "Player";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.CompareTag(tagName))
         {
             collision.transform.position = new Vector3(respawnPoint.transform.position.x, respawnPoint.transform.position.y, respawnPoint.transform.position.z);
         }
